66307be136afd7bf563cec6eb67ed9fce5e7ea67
myselfImu <utubediscow@gmail.com>
2021-09-23 01:24:44 +0800

    bank done

---
+++ scripts/first.js
@@ -0,0 +1,48 @@
+//login buttone event handler
+const lobinBtn = document.getElementById("login");
+lobinBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-area")
+    loginArea.style.display = "none";
+    const transectionArea = document.getElementById("transection-area");
+    transectionArea.style.display = "block";
+
+})
+
+//event button event handler
+const depositBtn =document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositNumber = getInputNumber("depositAmount");
+
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    
+    document.getElementById("depositAmount").value ="";
+})
+
+//withdraw button handler
+const withdrawBtn =document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+
+    updateSpanText("currentWithdraw",withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber)
+
+    document.getElementById("withdrawAmount").value ="";
+
+
+})
+
+
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+
+function updateSpanText (id ,depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
